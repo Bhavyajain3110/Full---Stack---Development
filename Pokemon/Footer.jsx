5d81ed9ec6fa48613536c13b26d7408bc9e74ec0
Bhavyajain3110 <111706073+Bhavyajain3110@users.noreply.github.com>
2024-03-14 01:53:39 +0530

    Update Footer.jsx

--- Pokemon/Footer.jsx
+++ Pokemon/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-        <h4>Made by Shekhar Tewani</h4>
+        <h4>Made by Bhavya jain</h4>
         
     </div>
   )
